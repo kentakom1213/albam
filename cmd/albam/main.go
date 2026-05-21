fc5548b3a21b95ad76c2235edaa9c8fd48e985fd
kentakom1213 <kentakom1213@gmail.com>
2026-05-21 23:47:42 +0900

    add: implement minimal cli

--- cmd/albam/main.go
+++ cmd/albam/main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: albam <command> [args]")
+		os.Exit(1)
+	}
+
+	cmd := os.Args[1]
+
+	switch cmd {
+	case "scan":
+		fmt.Println("scan command is not implemented yet")
+	default:
+		fmt.Fprintf(os.Stderr, "unknown command: %s\n", cmd)
+		os.Exit(1)
+	}
 }
